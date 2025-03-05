7fe4aacc7f76a3a06018580396c77e0f963e2937
Franziska Stude <fstude@MacBook-Air-von-Franziska.local>
2025-03-05 22:52:53 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  console.log("Calculating subtotal, yey!");
 
-  //... your code goes here
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+
+  const priceValue = parseFloat(price.innerText);
+  const quantityValue = parseInt(quantity.value);
+
+  const subtotalValue = priceValue * quantityValue;
+
+  const subtotalElement = product.querySelector(".subtotal span");
+  subtotalElement.innerText = subtotalValue.toFixed(2);
+
+  return subtotalValue;
 }
 
+//function calculateAll() {
+// code in the following two lines is added just for testing purposes.
+// it runs when only iteration 1 is completed. at later point, it can be removed.
+//const singleProduct = document.querySelector(".product");
+//updateSubtotal(singleProduct);
+// end of test
+
+// ITERATION 2
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const allProducts = document.querySelectorAll(".product");
 
-  // ITERATION 2
-  //... your code goes here
+  let total = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  allProducts.forEach((product) => {
+    total += updateSubtotal(product);
+  });
+
+  const totalElement = document.querySelector("#total-value span");
+  totalElement.innerText = total.toFixed(2);
+}
+
+// ITERATION 3
+function calculateAll() {
+  const allProducts = document.querySelectorAll(".product");
+  let total = 0;
+
+  allProducts.forEach((product) => {
+    total += updateSubtotal(product);
+  });
+
+  document.querySelector("#total-value span").innerText = total.toFixed(2);
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  console.log("The target in remove is:", target);
   //... your code goes here
 }
 
@@ -34,9 +63,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
   //... your code goes here
 });
